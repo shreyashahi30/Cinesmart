d6971514664c3eb8fe3b0b44e3d012695dc6ce82
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2026-09-20 15:45:01 +0530

    Update popular.js

--- static/popular.js
+++ static/popular.js
@@ -32,6 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <p>⭐ ${movie.vote_average}</p>
           `;
 
+          card.addEventListener("click", () => openMovieModal(movie.id));
+
           container.appendChild(card);
         });
       });
